61688149cb5263ed274c23daa47e41a45c7af4d1
Dane <dane@pop-os.localdomain>
2019-03-31 23:17:47 -0400

    Actually test and write first Red test

--- spec/simple_spec.rb
+++ spec/simple_spec.rb
@@ -5,3 +5,13 @@ describe HashArrayMask do
     expect(HashArrayMask.hi).to eql('Hello world!')
   end
 end
+
+describe Hash do
+  it 'responds to .merge' do
+    expect(Hash.new).to respond_to(:merge)
+  end
+  
+  it 'responds to .whitelist' do
+    expect(Hash.new).to respond_to(:whitelist)
+  end
+end
